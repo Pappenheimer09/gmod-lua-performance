82eb5946ff66b04dd9cccde87cd4fe50ca36ba29
Pappen <110184798+Pappenheimer09@users.noreply.github.com>
2026-02-26 19:49:02 +0100

    Revise benchmark data and add caching functionality
    Updated benchmark results for server and client with new cached values.

--- files/angle_zero_vs_new.lua
+++ files/angle_zero_vs_new.lua
@@ -1,21 +1,26 @@
 --[[
---- Benchmark complete
-On Server
-reps	30	rounds	5000
-Zero'd	1.3485066712595e-07
-Create	2.3365133318293e-07
---- Benchmark complete
-On Client
-reps	30	rounds	5000
-Zero'd	1.1277866673481e-07
-Create	2.4979333345679e-07
+	--- Benchmark complete
+	reps	30	rounds	5000
+	On Server
+	Zero'd	1.0690399993981e-07
+	Cached	5.7774666679128e-08
+	Create	3.0156200007203e-07
+	--- Benchmark complete
+	reps	30	rounds	5000
+	On Client
+	Zero'd	1.0355866663569e-07
+	Cached	5.5381333346152e-08
+	Create	1.8186866671461e-07
 --]]
-
+local ZeroCached =Angle(0,0,0)
 local ang = Angle(0,0,0)
 LuctusCompareOften(30,0.1,5000,{
     {"Zero'd",function()
         ang:Zero()
         return ang
+    end},
+		{"Cached",function()
+        return ZeroAng
     end},
     {"Create",function()
         return Angle(0,0,0)
